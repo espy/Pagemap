c66253c8d177239e61221e5af2731ded29d4b3ae
Alex Feyerke <kontakt@momoi.de>
2012-04-04 16:36:23 +0200

    Switched to points instead of ranges, complete rewrite

--- js/pagemap.js
+++ js/pagemap.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
 
-    $.fn.redrawForWebkit = function() {
-        this[0].style.display = 'none';
-        this[0].offsetHeight;
-        this[0].style.display = 'block';
-        console.log("fock");
-    };
-
+		$.fn.redrawForWebkit = function() {
+				this[0].style.display = 'none';
+				this[0].offsetHeight;
+				this[0].style.display = 'block';
+		};
 
 	init();
 });
+
 var wh = 0;
 var bh = 0;
 var ratio = 0;
-var selectors = ["h1"];
+var tooltipLength = 25;
+var selectors = ["h1", "h2", "p"];
+var container = "#main";
 var elements = [];
 var emptyPageMap = '<div id="pageIndicator"></div>';
 var config = {
@@ -24,30 +25,109 @@ function init(){
 	console.log("Pagemap init");
 	var pageMapMarkup = '<div id="pageMap"></div>';
 	$('body').append(pageMapMarkup);
+
+	parseSelectors();
+
+	$('#addSubline').live('click', function(event){
+		event.preventDefault();
+		$('<h2>H2 inserted afterwards</h2>').insertAfter(this);
+		setTimeout(parseSelectors, 1000);
+	});
+
 	$('#pagemap .clickTarget').live('click', function(event){
 		slideToElement(this);
 	});
+
+	$('#pagemap .clickTarget').live('mouseenter', function(event){
+		var preview = shorten($($(this).data('element')).text());
+		showPreview(preview, this);
+		console.log("over", preview);
+	});
+
+	$('#pagemap .clickTarget').live('mouseleave', function(event){
+		$('#pageMap #preview').hide();
+	});
+
+	/*
 	$(window).bind("resize", function(){
 		redrawMap();
 	});
+	redrawMap();
+	*/	
+
 	$(window).bind("scroll", function(){
 		drawScrollIndicator();
 	});
-	redrawMap();
 	drawScrollIndicator();
 }
+
+function showPreview(previewText, element) {
+	if(!$('#pageMap #preview').length){
+		$('#pageMap').append('<div id="preview"></div>');
+	}	
+	$('#pageMap #preview').text(previewText);
+	$('#pageMap #preview').show().css('top', $(element).offset().top - $('#pageMap').offset().top);
+}
+
+function parseSelectors(){
+	$.each(selectors, function(index, selector){
+		parseSelector(selector);
+	});
+	elements.sort(comparePositions);
+	console.log("Total elements",elements.length);
+	setTimeout(parseSelectors, 1500);
+}
+
+function parseSelector(selector) {
+	$.each($(selector, container), function(id, element){
+		if(!$(element).data('pageMap')){
+			var pageMapID = "pageMap_"+elements.length;
+			$(element).data('pageMap', pageMapID)
+			elements.push({
+				"id": pageMapID,
+				"element": element,
+				"tag": selector,
+				"pos": $(element).offset().top,
+				"pageMapElement": null
+			});
+		};
+	});
+	drawPageMap();	
+}
+
+function drawPageMap() {
+	//if(wh == $(window).height() && bh == $('body').height()) return;
+	wh = $(window).height();
+	bh = $('body').height();
+	ratio = wh / bh;
+	$.each(elements, function(id, element){
+		if(element.pageMapElement == null){
+			// create new element
+			var newElement = '<div class="clickTarget pageMap-'+element.tag+'"></div>';
+			$('#pageMap').append(newElement)
+			element.pageMapElement = $('#pageMap .pageMap-'+element.tag).last();
+			$(element.pageMapElement).data('element', element.element)
+			element.pos = $(element.element).offset().top;
+		} else {
+			element.pos = $(element.element).offset().top;
+		}
+		//$(element.pageMapElement).css('top', ratio * element.pos);
+		var target = Math.round(ratio * element.pos);
+		if(target != $(element.pageMapElement).offset().top){
+			$(element.pageMapElement).animate({	
+				top: target
+				}, 250
+			);
+		}
+	});
+	drawScrollIndicator();	
+}
+
+
 
 function slideToElement(source){
 	console.log("scrollTo ",$(source).data('element'));
 	$.scrollTo($(source).data('element'), config.scrollSpeed);
-	/* Old version:
-	for(id in elements){
-		if($(elements[id].mapElement).is(source)){
-			$.scrollTo(elements[id].element, config.scrollSpeed);
-			return;
-		}
-	}
-	*/
 }
 
 function redrawMap(){
@@ -57,7 +137,7 @@ function redrawMap(){
 	bh = $('body').height();
 	elements = [];
 	for(sid in selectors){
-		console.log("dealing with "+sid);
+		console.log("dealing with "+selectors[sid]);
 		$.each($(selectors[sid]), function(id, element){
 			var pos = $(element).offset().top
 			elements.push({
@@ -95,15 +175,16 @@ function drawElements(){
 		var element = elements[id];
 		var node = $('#pageMap .pageMap-'+element.tag)[id];
 		var elementMargin = parseInt($(node).css('marginBottom'));
-		console.log($(node), "margin: "+elementMargin);
 		var h = Math.ceil(element.sectionHeight * ratio) - elementMargin;
 		$(node).height(h);
 	}
-	console.log(elements.length+" elements found: ",elements);
 	drawScrollIndicator();	
 }
 
 function drawScrollIndicator(){
+	if(!$('#pageMap #pageIndicator').length){
+		$('#pageMap').append('<div id="pageIndicator"></div>');
+	}
 	var offset = $('body').scrollTop();
 	var percent = offset / (bh - wh);
 	if(percent < 0){percent = 0;}
@@ -111,3 +192,15 @@ function drawScrollIndicator(){
 	$('#pageMap #pageIndicator').height(wh * ratio).css('top', (percent * 100 * (1-ratio))+"%");
 }
 
+// Helpers
+ 
+function comparePositions(a, b) {
+	return a.pos - b.pos;
+}
+
+function shorten(text) {
+	var l = text.length;
+	text = text.substr(0,tooltipLength);
+	if(l > tooltipLength) text += "…";
+	return text;
+}
